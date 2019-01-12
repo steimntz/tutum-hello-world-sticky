7f86287c3a00c2f65c16b36793306650f76268b2
Steimntz M. de Figueiredo <steimntz@tjro.jus.br>
2019-01-12 12:13:35 -0400

    windsonizer the helloworld

--- www/index.php
+++ www/index.php
@@ -20,6 +20,18 @@
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
+	session_start();
+ 
+	if(isset($_SESSION["windson"])) {
+		?>
+	<b>Windsonized</b>
+		<?php
+	}
+
+	if(!isset($_SESSION["windson"])){
+	    $_SESSION["windson"] = "windsonizer";
+	}
+
 	$links = [];
 	foreach($_ENV as $key => $value) {
 		if(preg_match("/^(.*)_PORT_([0-9]*)_(TCP|UDP)$/", $key, $matches)) {
